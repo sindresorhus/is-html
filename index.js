c29bd4e8d1c0bbc1b94051d44f7f9b58d34b0fab
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-22 22:59:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const simple = /^\s*</;
 const basic = /\s?<!doctype html>|(<html\b[^>]*>|<body\b[^>]*>|<x-[^>]+>)+/i;
 let full;
 
-module.exports = string => simple.test(string) || basic.test(string) || full || (full = new RegExp(require('html-tags').map(tag => `<${tag}\\b[^>]*>`).join('|'), 'i'));
+module.exports = string =>
+	basic.test(string) ||
+	(full || (full = new RegExp(require('html-tags').map(tag => `<${tag}\\b[^>]*>`).join('|'), 'i'))).test(string);
