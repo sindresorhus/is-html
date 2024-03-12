98494824979057fdef16e0cd69331a4cac5a09dc
Han Yeong-woo <han@yeongwoo.dev>
2024-03-12 19:17:04 +0900

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/**
+Check if a string is HTML.
+
+@param string - The string to check if it is HTML.
+@returns `true` if the string is HTML, `false` otherwise.
+*/
+export default function isHtml(string: string): boolean;
